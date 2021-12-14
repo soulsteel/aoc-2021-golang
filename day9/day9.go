ea4a966c0123e919407d78b219ace70230934b4a
soulsteel <soulsteel1993@gmail.com>
2021-12-14 14:04:26 +0100

    day 9

--- day9/day9.go
+++ day9/day9.go
@@ -1,88 +1,120 @@
 package day9
 
 import (
-	"fmt"
 	"github.com/soulsteel/aoc-2019-golang/utils"
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func GetResults() {
 	input := utils.ReadLines("./input/day9.txt")
-	utils.PrintReport(partOne(input),0,9)
+	grid := createGrid(input)
+	lowPoints := findLowPoints(grid)
+
+	utils.PrintReport(partOne(grid, lowPoints), partTwo(grid, lowPoints), 9)
 }
 
-func partOne(input []string) int {
-	lowPoints := 0
-	grid, lineLength := makeGrid(input)
+func partOne(grid [][]int, lowPoints [][]int) (count int) {
+	for _, coords := range lowPoints {
+		count += grid[coords[0]][coords[1]] + 1
+	}
+	return count
+}
 
-	for i:=0;i<len(grid);i++{
-		if i == 0 {	// top left
-			if grid[i] < grid[i+1] && grid[i] < grid[i+lineLength] {
-				lowPoints += grid[i]+1
-				//fmt.Println(i, grid[i], "top left")
+func partTwo(grid [][]int, lowPoints [][]int) int {
+	visited := make([][]bool, 0)
+	for i:=0;i<len(grid);i++ {
+		row := make([]bool, 0)
+		for j:=0;j<len(grid[0]);j++ {
+			row = append(row, false)
+		}
+		visited = append(visited, row)
+	}
+
+	adjX := []int{-1, 0, 1, 0}
+	adjY := []int{0, 1, 0, -1}
+	largeBasins := make([]int, 0)
+
+	for _, v := range lowPoints {
+		queue := make([][]int, 0)
+		queue = append(queue, v)
+		visited[v[0]][v[1]] = true
+		basinCounter := 1
+
+		for len(queue) > 0 {
+			firstOut := queue[0]
+			queue = queue[1:]
+
+			for k:=0; k<4; k++ {
+				adjI, adjJ := firstOut[0] + adjX[k], firstOut[1] + adjY[k]
+
+				if adjI >= 0 && adjI < len(grid) &&
+					adjJ >= 0 && adjJ < len(grid[0]) &&
+					!visited[adjI][adjJ] &&
+					grid[adjI][adjJ] != 9 {
+					queue = append(queue, []int{adjI, adjJ})
+					visited[adjI][adjJ] = true
+					basinCounter++
+				}
 			}
-		} else if i == lineLength-1 { // top right
-			if grid[i] < grid[i-1] && grid[i] < grid[i+lineLength] {
-				lowPoints += grid[i]+1
-				//fmt.Println(i, grid[i], "top right")
+		}
+
+		if len(largeBasins) == 3 {
+			sort.Ints(largeBasins)
+			if largeBasins[0] < basinCounter {
+				largeBasins[0] = basinCounter
 			}
-		} else if i == len(grid) - lineLength { // bottom left
-			if grid[i] < grid[i-lineLength] && grid[i] < grid[i+1] {
-				lowPoints += grid[i]+1
-				//fmt.Println(i, grid[i], "bottom left")
-			}
-		} else if i == len(grid) - 1 { // bottom right
-			if grid[i] < grid[i-lineLength] && grid[i] < grid[i-1] {
-				lowPoints += grid[i]+1
-				//fmt.Println(i, grid[i], "bottom right")
-			}
-		} else if i > 0 && i < lineLength { // top margin
-			if grid[i] < grid[i-1] && grid[i] < grid[i+1] && grid[i] < grid[i+lineLength] {
-				lowPoints += grid[i]+1
-				//fmt.Println(i, grid[i], "top margin")
-			}
-		}  else if i > len(grid) - lineLength && i < len(grid) - 1 { // bottom margin
-			if grid[i] < grid[i-lineLength] && grid[i] < grid[i-1] && grid[i] < grid[i+1] {
-				lowPoints += grid[i]+1
-				//fmt.Println(i, grid[i], "bottom margin")
-			}
-		} else if i % lineLength == 0 { // left margin
-			if grid[i] < grid[i+1] && grid[i] < grid[i-lineLength]  && grid[i] < grid[i+lineLength] {
-				lowPoints += grid[i]+1
-				//fmt.Println(i, grid[i], "left margin")
-			}
-		} else if (i - lineLength + 1) % lineLength == 0 { // right margin
-			if grid[i] < grid[i-1] && grid[i] < grid[i-lineLength] && grid[i] < grid[i+lineLength] {
-				lowPoints += grid[i]+1
-				//fmt.Println(i, grid[i], "right margin")
-			}
-		} else { // every other point on the grid
-			if grid[i] < grid[i+1] && grid[i] < grid[i-1] && grid[i] < grid[i-lineLength] && grid[i] < grid[i+lineLength] {
-				lowPoints += grid[i]+1
-				//fmt.Println(i, grid[i], "grid")
-			}
+		} else {
+			largeBasins = append(largeBasins, basinCounter)
 		}
 	}
 
-	fmt.Println(lowPoints)
-
-	return 0
+	return largeBasins[0] * largeBasins[1] * largeBasins[2]
 }
 
-func makeGrid(input []string) (A []int, lineLength int) {
-	lineLength = len(input[0])
-	for _, v := range input {
-		splitLine := strings.Split(v, "")
-		for _, numberStr := range splitLine {
-			number, err := strconv.Atoi(numberStr)
+func createGrid(input []string) [][]int {
+	grid := make([][]int, 0)
+
+	for _, line := range input {
+		row := strings.Split(line, "")
+		numbers := make([]int, 0)
+		for _, numberStr := range row {
+			n, err := strconv.Atoi(numberStr)
 			if err != nil {
 				log.Fatalln(err)
 			}
-			A = append(A, number)
+			numbers = append(numbers, n)
+		}
+		grid = append(grid, numbers)
+	}
+
+	return grid
+}
+
+func findLowPoints(grid [][]int) [][]int {
+	adjX := []int{-1, 0, 1, 0}
+	adjY := []int{0, 1, 0, -1}
+
+	lowPoints := make([][]int, 0)
+
+	for i:=0;i<len(grid);i++ {
+		for j:=0;j<len(grid[0]);j++ {
+			isLowPoint := true
+			for k:=0;k<4;k++ {
+				adjI, adjJ := i + adjX[k], j + adjY[k]
+				if adjI >= 0 && adjI < len(grid) && adjJ >= 0 && adjJ < len(grid[0]) {
+					if grid[adjI][adjJ] <= grid[i][j] {
+						isLowPoint = false
+					}
+				}
+			}
+			if isLowPoint {
+				lowPoints = append(lowPoints, []int{i, j})
+			}
 		}
 	}
 
-	return A, lineLength
+	return lowPoints
 }
